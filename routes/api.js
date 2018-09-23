d9710f892c8ae56ed1bfbfdd6aa1ee5465c9fc85
Glitch (chestnut-reaction) <none>
2018-09-23 23:15:03 +0000

    ❣️💡 Checkpoint ./routes/api.js:1744758/1023 ./package.json:1744758/25

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,18 @@ var ObjectId = require('mongodb').ObjectID;
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.connect(CONNECTION_STRING);
+
+var schema = new Schema({
+  name : String,
+  age : Number,
+  favoriteFoods : [String]
+});
+
+var Person = mongoose.model('Person', schema);
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -29,8 +41,14 @@ module.exports = function (app) {
       let title = req.body.issue_title;
       let text = req.body.issue_text;
       let by = req.body.created_by;
-      if (
-      //console.log(title + ", " + text + ", " + by);
+      let assignedTo = req.body.assigned_to;
+      let statusText = req.body.status_text;
+      console.log('sT: ' + statusText);
+      var createAndSavePerson = function(done) {
+        const person = new Person({name: "dave", age: "30", favoriteFoods: ["Burgers"]});
+        person.save((err, data) => (err ? done(err) : done(null, data)));
+        //done(null /*, data*/);
+      };
     })
     
     .put(function (req, res){
